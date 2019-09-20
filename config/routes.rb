9b7fcd60a7a5f23f849c5ddf873c505190951b61
Rafael Rodrigues <rafael.silva@synergiaconsultoria.com.br>
2019-09-20 16:52:35 -0300

    Adiciona tabela Clientes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :clients
   resources :tasks
   resources :labor_processes
   resources :administrative_processes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190919014853) do
+ActiveRecord::Schema.define(version: 20190920182749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20190919014853) do
     t.boolean  "status"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.string   "email"
+    t.boolean  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "labor_processes", force: :cascade do |t|
