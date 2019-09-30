b09cf6ae2aa100f0d908d87d4465ac6561dc3d0a
Rafael Silva <rafael.r.silva1996@gmail.com>
2019-09-29 22:47:55 -0300

    Refatora views da tabela Clientes para aceitar novos campos

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -69,6 +69,6 @@ class ClientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def client_params
-      params.require(:client).permit(:name, :age, :email, :status)
+      params.require(:client).permit(:name, :age, :email, :status, :cep, :uf, :city, :neighborhood, :address, :number, :father, :mother, :marital_status, :naturalness, :uf_naturalness, :cpf, :issuing_organ, :document_type, :contact, :scrap_contact, :relationship1, :relationship2, :observation)
     end
 end
--- app/views/clients/_client.json.jbuilder
+++ app/views/clients/_client.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! client, :id, :name, :age, :email, :status, :created_at, :updated_at
+json.extract! client, :id, :name, :age, :email, :cep, :uf, :city, :neighborhood, :address, :number, :father, :mother, :marital_status, :naturalness, :uf_naturalness, :cpf, :issuing_organ, :document_type, :contact, :scrap_contact, :relationship1, :relationship2, :observation, :status, :created_at, :updated_at
 json.url client_url(client, format: :json)
