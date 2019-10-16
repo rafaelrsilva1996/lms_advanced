21fabfe676f9c38446aa83bd38b99e38c9ad2846
Rafael Silva <rafael.r.silva1996@gmail.com>
2019-10-15 22:35:40 -0300

    Adiciona controlers e routes da Users para aplicar as funções de json

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,16 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root 'home#index'
+
+  namespace :user do
+    resources :problems
+    resources :services
+    devise_scope :user do
+      post "/sign_in", :to => 'sessions#create'
+      post "/sign_up", :to => 'registrations#create'
+      delete "/sign_out", :to => 'sessions#destroy'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
