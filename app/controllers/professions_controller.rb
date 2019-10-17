487ec7bcd32c5c79637f55fcbb3922e098cdf0be
EnzoDomingues <enzo.dominguees@gmail.com>
2019-10-17 17:23:32 -0300

    make it better

--- app/controllers/professions_controller.rb
+++ app/controllers/professions_controller.rb
@@ -1,6 +1,6 @@
 class ProfessionsController < ApplicationController
   before_action :set_profession, only: [:show, :edit, :update, :destroy]
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   # GET /professions
   # GET /professions.json
